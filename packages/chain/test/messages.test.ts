37d106dadf01addb8181d5f63557e1b67fbc79b9
berkingurcan <berkingurcan@yandex.com>
2024-04-25 20:36:56 +0300

    Update messages.test.ts

--- packages/chain/test/messages.test.ts
+++ packages/chain/test/messages.test.ts
@@ -1,6 +1,6 @@
-import { AppChain, TestingAppChain } from "@proto-kit/sdk";
+import { TestingAppChain } from "@proto-kit/sdk";
 import { Field, PrivateKey, UInt64 } from "o1js";
-import { Messages, Agent } from "../src/messages";
+import { Messages, Agent, Message, MessageDetail } from "../src/messages";
 import { log } from "@proto-kit/common";
 
 log.setLevel("ERROR");
@@ -52,11 +52,33 @@ describe("Mina Spy Chain Messages", () => {
         await tx1.sign();
         await tx1.send();
 
-        const block = await appChain.produceBlock();
+        await appChain.produceBlock();
 
         const agent = await appChain.query.runtime.Messages.existingAgents.get(agents[0].agentId)
         expect(agent?.agentId).toEqual(agents[0].agentId);
         expect(agent?.lastMessageNumber).toEqual(agents[0].lastMessageNumber);
         expect(agent?.securityCode).toEqual(agents[0].securityCode);
     })
+
+    it("Process a valid message", async () => {
+        const validMessage = new Message({
+            messageNumber: Field(1),
+            messageDetails: new MessageDetail({
+                agent: agents[0],
+                message: Field(100000000001)
+            })
+        });
+    
+        const tx2 = await appChain.transaction(carrieMathison, () => {
+            messages.processMessage(validMessage);
+        });
+    
+        await tx2.sign();
+        await tx2.send();
+        await appChain.produceBlock();
+    
+        const updatedAgent = await appChain.query.runtime.Messages.existingAgents.get(agents[0].agentId);
+        expect(updatedAgent?.lastMessageNumber).toEqual(Field(1));
+    });
+    
 })
